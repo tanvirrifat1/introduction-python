d1f3609878d0dd2928523707cc06a9e8d4cba582
tanvirrifat1 <rifatkhan5567790@gmail.com>
2026-04-07 16:46:44 +0600

    added more

--- first.py
+++ first.py
@@ -1,7 +1,30 @@
-a = 3
+# numbers = [1, 2, 3, 4, 5]
 
 
-if a > 5:
-    print("a is greater than 5")
-else:
-    print("a is less than or equal to 5")
+# for num in numbers:
+#     print(num)
+
+
+# for i in range(1, 6, 3):
+#     print(i)
+
+
+# numbrs = [100, 500, 200]
+
+
+# largeValue = max(numbrs)
+# print(largeValue)
+
+
+def double(x):
+    return x * 2
+
+
+print(double(5))
+
+
+def sum(num1, num2):
+    return num1 + num2
+
+
+print(sum(2, 3))
