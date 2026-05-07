78cad96d1d2a0b308d6ab219ccbd4adffd71f261
tanvirrifat1 <rifatkhan5567790@gmail.com>
2026-05-07 22:00:22 +0600

    added

--- person.py
+++ person.py
@@ -1,3 +1,8 @@
+import random
+
+from school import School
+
+
 class Person:
     def __init__(self, name):
         self.name = name
@@ -10,5 +15,32 @@ class Teacher(Person):
     ):
         super().__init__(name)
 
-    def evalution_exam(self):
+    def evaluate_exam(self):
         return random.randint(1, 100)
+
+
+class Student(Person):
+    def __init__(self, name, classroom):
+        super().__init__(name)
+        self.classroom = classroom
+        self.__id = None
+        self.marks = {}
+        self.subject_grade = {}
+        self.grade = None
+
+    def final_grade(self):
+        sum = 0
+        for grade in self.subject_grade.values():
+            point = School.grade_to_value(grade)
+            sum = sum + point
+
+        gpa = sum / len(self.subject_grade)
+        self.grade = School.value_to_grade(gpa)
+
+        @property
+        def id(self):
+            return self.__id
+
+        @id.setter
+        def id(self, value):
+            self.__id = value
--- shop.py
+++
@@ -1,7 +0,0 @@
-choice = int(input("enter your choice : "))
-
-if choice == 1:
-    print("hello")
-
-elif choice == 2:
-    print("hi")
